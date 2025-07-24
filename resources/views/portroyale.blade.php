4a8eb07f39c1855c16b55494f24929b19050dced
Khanley Mesa <khanleymesa@gmail.com>
2025-07-25 07:31:56 +0800

    Modal edited

--- resources/views/portroyale.blade.php
+++ resources/views/portroyale.blade.php
@@ -908,7 +908,7 @@
                 <div class="modal-header">
                     <h5 class="modal-title" id="summaryModalLabel">
                         <i class="bi bi-stars"></i>
-                        Review Summary
+                        Total Reviews Summarized: {{ $totalReviews }}
                     </h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">X</button>
                 </div>
